7979793b04ff2232240e036b06dbdffcfa6e82e2
Onika Chorba <110539259+onikaChorba@users.noreply.github.com>
2025-12-01 19:42:37 +0200

    first commit, add header

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import './App.css';
+import { Layout } from 'antd';
+import { HeaderCustom } from './components/header/header';
+const { Content, Footer } = Layout;
+
+function App() {
+
+  return (
+    <Layout>
+      <HeaderCustom />
+
+      <Content>fgdmshm</Content>
+      <Footer >Foter</Footer>
+    </Layout>
+  )
+}
+
+export default App
