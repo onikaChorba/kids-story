dad35336233490ca3a43dee78f72dba032fc4aed
Onika Chorba <110539259+onikaChorba@users.noreply.github.com>
2026-02-02 21:40:02 +0200

    feat: sing form

--- src/section/hero/hero.tsx
+++ src/section/hero/hero.tsx
@@ -1,55 +1,121 @@
-import './index.css'
-import { icons } from '../../icons'
-import { Button, Flex, Form, Input, Radio } from 'antd'
+import './index.css';
+import { icons } from '../../icons';
+import { Button, Flex, Form, Input, Radio, Typography, message } from 'antd';
+
+const { Title, Paragraph } = Typography;
+
+interface FormValues {
+  name: string;
+  phone: string;
+  email: string;
+  agreement: boolean;
+}
 
 const Hero = () => {
+  const [form] = Form.useForm();
+
+  const onFinish = (values: FormValues) => {
+    alert(`Success! Data sent:\n
+      Name: ${values.name}
+      Phone: ${values.phone}
+      Email: ${values.email}
+    `);
+
+    form.resetFields();
+    message.success('Registration successful!');
+  };
+
   return (
-    <Flex align='center' justify='center' gap="small" wrap style={{ position: 'relative', marginTop: '100px' }} className='hero'>
+    <Flex align='center' justify='center' gap="small" wrap className='hero'>
       <Flex vertical className='textblock' gap="small">
         <h1>Kidsstory</h1>
-        <h2>English for children
-          garden and club</h2>
+        <h2>English for children garden and club</h2>
       </Flex>
-      <img src={icons.heroImg} width="100%" />
-      <Form style={{ position: 'absolute', right: '0px' }} className='form'>
-        <Flex vertical>
-          <h2>Sign up
-            on personal
-            excursion</h2>
-          <p>Fill out the form and see
-            I use the kindergarten in a convenient way
-            it's your time</p>
-          <Flex vertical gap="small">
-            <Input
-              type="text"
-              placeholder="Your name"
-              size="large"
-              className='input'
-              suffix={<span className="required-star" onClick={() => console.log('star clicked')}>*</span>}
-            />
-            <Input
-              type="tel"
-              placeholder="+ 3 _  _  _  -  _  _  _  -  _  _  -  _  _"
-              size="large"
-              className='input'
-              suffix={<span className="required-star" onClick={() => console.log('star clicked')}>*</span>}
-            />
-            <Input
-              type="email"
-              placeholder="Your email"
-              size="large"
-              className='input'
-              suffix={<span className="required-star" onClick={() => console.log('star clicked')}>*</span>}
-            />
-            <Radio className='radio'>I accept the terms
-              user agreement</Radio>
 
-            <Button variant="text" type="primary" className="primary-orange-bg button"> sign up</Button>
+      <img src={icons.heroImg} width="100%" alt="Hero" />
+
+      <Form
+        form={form}
+        className='form'
+        onFinish={onFinish}
+        layout="vertical"
+        initialValues={{ agreement: false }}
+      >
+        <Flex vertical>
+          <Title level={2}>Sign up on personal excursion</Title>
+          <Paragraph>
+            Fill out the form and see the kindergarten in a convenient time
+          </Paragraph>
+
+          <Flex vertical gap="small">
+            <Form.Item
+              name="name"
+              rules={[{ required: true, message: 'Please enter your name' }]}
+            >
+              <Input
+                placeholder="Your name *"
+                size="large"
+                className='input'
+              />
+            </Form.Item>
+
+            <Form.Item
+              name="phone"
+              rules={[
+                { required: true, message: 'Please enter your phone number' },
+                { pattern: /^\+?3?8?(0\d{9})$/, message: 'Invalid phone format' }
+              ]}
+            >
+              <Input
+                type="tel"
+                placeholder="+380 _ _  _ _ _  _ _  _ _ *"
+                size="large"
+                className='input'
+              />
+            </Form.Item>
+
+            <Form.Item
+              name="email"
+              rules={[
+                { required: true, message: 'Please enter your email' },
+                { type: 'email', message: 'The input is not valid E-mail!' }
+              ]}
+            >
+              <Input
+                placeholder="Your email *"
+                size="large"
+                className='input'
+              />
+            </Form.Item>
+
+            <Form.Item
+              name="agreement"
+              valuePropName="checked"
+              rules={[
+                {
+                  validator: (_, value) =>
+                    value ? Promise.resolve() : Promise.reject(new Error('Should accept agreement')),
+                },
+              ]}
+            >
+              <Radio className='radio'>
+                I accept the terms user agreement
+              </Radio>
+            </Form.Item>
+
+            <Button
+              type="primary"
+              htmlType="submit"
+              className="primary-orange-bg button"
+              block
+            >
+              sign up
+            </Button>
           </Flex>
         </Flex>
       </Form>
     </Flex>
-  )
-}
+  );
+};
 
-export { Hero }
+export { Hero };
